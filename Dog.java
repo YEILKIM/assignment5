c941a5bb498f43c2ce4cd90aded4680481818d00
YEILKIM <31638901+YEILKIM@users.noreply.github.com>
2017-09-17 14:13:22 +0900

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,12 @@
+
+public class Dog extends Animal{
+	public Dog(){
+		System.out.println("A new dog has been created!");
+	}
+	public void sleep(){
+		System.out.println("A dog sleeps...");
+	}
+	public void eat(){
+		System.out.println("A dog eats...");
+	}
+}
